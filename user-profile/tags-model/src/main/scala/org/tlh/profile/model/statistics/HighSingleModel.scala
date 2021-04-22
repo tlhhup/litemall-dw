cd6ac461626f2adb31052fd65b54bd4366a3c753
tlhhup <137273278@qq.com>
2021-04-22 11:32:32 +0800

    1.[feat]: add The highest single model and per customer transaction model

---
+++ user-profile/tags-model/src/main/scala/org/tlh/profile/model/statistics/HighSingleModel.scala
@@ -0,0 +1,72 @@
+package org.tlh.profile.model.statistics
+
+import org.apache.spark.sql.types.DecimalType
+import org.apache.spark.sql.{Column, DataFrame}
+import org.tlh.profile.entity
+import org.tlh.profile.entity.CommonMeta
+import org.tlh.profile.model.SingleMetaModel
+
+/**
+  * 单比最高
+  *
+  * @author 离歌笑
+  * @desc
+  * @date 2021-04-22
+  */
+object HighSingleModel extends SingleMetaModel {
+
+  def main(args: Array[String]): Unit = {
+    start()
+  }
+
+  /**
+    * 应用名称
+    *
+    * @return
+    */
+  override def getAppName(): String = "HighSingleModel"
+
+  /**
+    * 获取标签名称
+    *
+    * @return
+    */
+  override def getTagName(): String = "单笔最高"
+
+  /**
+    * 业务逻辑处理
+    *
+    * @param rules
+    * @param commonMeta
+    * @param source
+    * @return
+    */
+  override def processDetail(rules: Array[entity.Tag], commonMeta: CommonMeta, source: DataFrame): DataFrame = {
+    import source.sparkSession.implicits._
+    import org.apache.spark.sql.functions._
+
+    //1. 计算单比最高
+    val singleMax = max('order_price) as 'orderMax
+    val tmp = source
+      .select('user_id, 'order_price.cast(DecimalType.USER_DEFAULT)) // 转换数据类型(hBase中为字符串)
+      .groupBy('user_id)
+      .agg(singleMax)
+
+    //2. 构建匹配条件
+    var condition: Column = null
+    rules.foreach(item => {
+      val Array(start, end) = item.rule.split("-")
+      condition = if (condition == null) {
+        when('orderMax.between(start, end), item.name)
+      } else {
+        condition.when('orderMax.between(start, end), item.name)
+      }
+    })
+
+    //3. 执行计算
+    val result = tmp.select('user_id as 'id, condition as 'singleOrderMax)
+      .filter('singleOrderMax isNotNull)
+
+    result
+  }
+}
---
+++ user-profile/tags-model/src/main/scala/org/tlh/profile/model/statistics/PCTModel.scala
@@ -0,0 +1,74 @@
+package org.tlh.profile.model.statistics
+
+import org.apache.spark.sql.{Column, DataFrame}
+import org.tlh.profile.entity
+import org.tlh.profile.entity.CommonMeta
+import org.tlh.profile.model.SingleMetaModel
+
+/**
+  * per customer transaction 客单价 每一个顾客平均购买商品的金额，也即是平均交易金额
+  * <br>
+  * 客单价=销售额÷成交顾客数
+  *
+  * @author 离歌笑
+  * @desc
+  * @date 2021-04-22
+  */
+object PCTModel extends SingleMetaModel {
+
+  def main(args: Array[String]): Unit = {
+    start()
+  }
+
+  /**
+    * 应用名称
+    *
+    * @return
+    */
+  override def getAppName(): String = "PCTModel"
+
+  /**
+    * 获取标签名称
+    *
+    * @return
+    */
+  override def getTagName(): String = "客单价"
+
+  /**
+    * 业务逻辑处理
+    *
+    * @param rules
+    * @param commonMeta
+    * @param source
+    * @return
+    */
+  override def processDetail(rules: Array[entity.Tag], commonMeta: CommonMeta, source: DataFrame): DataFrame = {
+    import source.sparkSession.implicits._
+    import org.apache.spark.sql.functions._
+
+    //1.计算销售额  成交顾客数
+    val orderAmount = sum(when('order_status === 401, 'order_price).otherwise(0)) as 'orderAmount
+    val competeOrder = sum(when('order_status === 401, 1).otherwise(0)) as 'competeOrder
+    //2.计算客单价
+    val pct = orderAmount.divide(competeOrder) as 'pct
+    val temp = source.select(pct)
+
+    //3.构建条件
+    var condition: Column = null
+    rules.foreach(item => {
+      val Array(start, end) = item.rule.split("-")
+      condition = if (condition == null) {
+        when('pct.between(start, end), item.name)
+      } else {
+        condition.when('pct.between(start, end), item.name)
+      }
+    })
+
+    //4.执行计算
+    val tmp = temp.select(condition as 'pct)
+
+    val result = source.crossJoin(tmp).select('user_id as 'id, 'pct)
+
+    result
+  }
+}
